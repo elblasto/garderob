ab28c4300875fb158a7451c7e8c74b5381824cf7
Timofey <timlefff@gmail.com>
2014-02-03 21:48:33 +0800

    Normilize files

--- admi/config.php
+++ admi/config.php
@@ -8,11 +8,11 @@ define('HTTPS_SERVER', 'http://garderob/admi/');
 define('HTTPS_CATALOG', 'http://garderob/');
 
 // DIR
-define('DIR_APPLICATION', 'C:\11111\OpenServer\domains\garderob/admin/');
+define('DIR_APPLICATION', 'C:\11111\OpenServer\domains\garderob/admi/');
 define('DIR_SYSTEM', 'C:\11111\OpenServer\domains\garderob/system/');
 define('DIR_DATABASE', 'C:\11111\OpenServer\domains\garderob/system/database/');
-define('DIR_LANGUAGE', 'C:\11111\OpenServer\domains\garderob/admin/language/');
-define('DIR_TEMPLATE', 'C:\11111\OpenServer\domains\garderob/admin/view/template/');
+define('DIR_LANGUAGE', 'C:\11111\OpenServer\domains\garderob/admi/language/');
+define('DIR_TEMPLATE', 'C:\11111\OpenServer\domains\garderob/admi/view/template/');
 define('DIR_CONFIG', 'C:\11111\OpenServer\domains\garderob/system/config/');
 define('DIR_IMAGE', 'C:\11111\OpenServer\domains\garderob/image/');
 define('DIR_CACHE', 'C:\11111\OpenServer\domains\garderob/system/cache/');
